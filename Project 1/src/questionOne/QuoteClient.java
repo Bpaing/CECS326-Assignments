b309a7e45873bfdf55b1e63102bd682672da5adf
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-09-24 21:18:06 -0700

    Some work
    Need to do more research on input and output streams for the server.

--- Project 1/src/questionOne/QuoteClient.java
+++ Project 1/src/questionOne/QuoteClient.java
@@ -3,12 +3,16 @@ import java.net.*;
 import java.io.*;
 
 //accept() method waits for a client to connect
-class DateClient
+class QuoteClient
 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         try {
-                Socket client = new Socket("localhost", 6017);
+            Socket client = new Socket("localhost", 6017);
+            while (true)
+            {
 
+
+            }
         }
         catch (IOException ioe) {
             System.err.println(ioe);
--- Project 1/src/questionOne/QuoteServer.java
+++ Project 1/src/questionOne/QuoteServer.java
@@ -2,16 +2,17 @@ package questionOne;
 import java.net.*;
 import java.io.*;
 
-class DateServer
+class QuoteServer
 {
     public static void main(String[] args) throws IOException {
         try {
             ServerSocket sock = new ServerSocket(6017);
-            System.out.println("Server Started");
+            System.out.println("Server started, waiting for client...");
             while (true) {
-                //accept() method stalls while waiting for a client to connect
                 Socket client = sock.accept();
+                System.out.println("New client accepted.");
 
+                FileInputStream quotes = new FileInputStream(new File("questionOne/quotes.txt"));
                 PrintWriter pout = new
                         PrintWriter(client.getOutputStream(), true);
 
@@ -24,4 +25,5 @@ class DateServer
             System.err.println(ioe);
         }
     }
+
 }
