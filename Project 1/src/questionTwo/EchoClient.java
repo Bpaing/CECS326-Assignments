f2e2d6346d579b28670defe711ef64ac3cd030b4
brianhuyphung <brianhuyphung@gmail.com>
2021-09-25 20:16:16 -0700

    Client/Server
    Works for Server Responses.

--- Project 1/src/questionTwo/EchoClient.java
+++ Project 1/src/questionTwo/EchoClient.java
@@ -4,28 +4,26 @@ import java.net.*;
 import java.io.*;
 public class EchoClient {
 
-    public static void main(String[] args) throws IOException {
-        try {
-            Socket server = new Socket("localhost", 0125);
-
-            PrintWriter pout = new PrintWriter(server.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-
-            String userInput;
-            while((userInput = stdIn.readLine()) != null) {//read user input from standard System.in
-                pout.println(userInput);                    //write that in socket stream
-                System.out.println(in.readLine());            //read from socket stream
-            }
-            server.close();
-        }
-        catch(UnknownHostException e){
-            System.err.println(e.getMessage());
-        }
-        catch(IOException e) {
-            System.err.println(e.getMessage());
-        }
-
-    }
-
+	public static void main(String[] args) throws IOException {
+		try {
+			Socket server = new Socket("localhost", 0125);
+			
+			PrintWriter pout = new PrintWriter(server.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			
+			String userInput;
+			while((userInput = stdIn.readLine()) != null) {
+				pout.println(userInput);				
+				System.out.println("Server:" + in.readLine());			
+			}
+			server.close();
+		}
+		catch(UnknownHostException e){
+			System.err.println(e.getMessage());
+		}
+		catch(IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
 }
