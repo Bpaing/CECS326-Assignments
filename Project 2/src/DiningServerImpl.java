23e65cdab244d04450de4cc146c99616db314d60
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-10-13 14:20:04 -0700

    Project 2 setup
    Added files related to project 2.

---
+++ Project 2/src/DiningServerImpl.java
@@ -0,0 +1,24 @@
+/**
+ * DiningServer.java
+ *
+ * This class contains the methods called by the  philosophers.
+ *
+ */
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DiningServerImpl  implements DiningServer
+{
+    @Override
+    public void takeForks(int philNumber) {
+
+    }
+
+    @Override
+    public void returnForks(int philNumber) {
+
+    }
+    //Your code here
+}
---
+++ Project 2/src/Philosopher.java
@@ -0,0 +1,17 @@
+/**
+ * Philosopher.java
+ *
+ * This class represents each philosopher thread.
+ * Philosophers alternate between eating and thinking.
+ *
+ */
+
+
+public class Philosopher implements Runnable
+{
+    //Your code here
+    @Override
+    public void run() {
+
+    }
+}
