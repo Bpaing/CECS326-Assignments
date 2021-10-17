359bf28981652f65ee72e539f771c8fb9ec1f254
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-10-16 17:27:59 -0700

    DiningPhilosophers update
    Added more stuff to main method. Entire problem is set up, now need to implement mutex logic

--- Project 2/src/DiningPhilosophers.java
+++ Project 2/src/DiningPhilosophers.java
@@ -11,6 +11,21 @@ public class DiningPhilosophers
     public static void main(String args[])
     {
         Philosopher[] phil = new Philosopher[5];
-        Object forks = new Object[5];
+        Object[] forks = new Object[5];
+
+        for (int i = 0; i < forks.length; i++)
+            forks[i] = new Object();
+
+        for (int i = 0; i < phil.length; i++) {
+            Object left = forks[i];
+            Object right = forks[(i+1 % forks.length)];
+            //modulus length so that the last philosopher can use the first fork
+
+            phil[i] = new Philosopher(left, right);
+
+            Thread thread = new Thread(phil[i], "Philosopher #" + (i+1));
+            thread.start();
+            // now every philosopher is executing run() method
+        }
     }
 }
