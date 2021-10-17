dd602da869b1aa120213836beb9174d1e819124f
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-10-16 17:30:46 -0700

    Update Philosopher.java

--- Project 2/src/Philosopher.java
+++ Project 2/src/Philosopher.java
@@ -17,27 +17,20 @@ public class Philosopher implements Runnable
         rightFork = right;
     }
 
-    private void doSomething(String str) {
-        System.out.printf("%s is currently %s", Thread.currentThread().getName(), str);
-    }
-
     /*
             "Runnable interface is the primary template for any object that is intended to be executed by a thread.
             It defines a single method run(), which is meant to contain the code that is executed by the thread."
-
-            define threads in main?
      */
     @Override
     public void run() {
         while (true)
         {
             //think
-            doSomething("thinking");
-            //pick up either left or right
-
-            //eat (when both picked up)
-            doSomething("eating");
+            //pick up forks
+                //takeForks()
+            //eat
             //put down
+                //returnForks
         }
     }
 }
