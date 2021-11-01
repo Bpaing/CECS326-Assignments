075e90b16c974d60950a6555477be87019af2967
brianhuyphung <brianhuyphung@gmail.com>
2021-10-31 18:24:29 -0700

    Update VillageRoad.java
    Finishing touches

--- Project 3/src/VillageRoad.java
+++ Project 3/src/VillageRoad.java
@@ -2,6 +2,12 @@ public class VillageRoad
 {
     public static void main(String[] args)
     {
+        EastVillage east = new EastVillage();
+        WestVillage west = new WestVillage();
 
+        Thread thread = new Thread(east, "East Person");
+        Thread thread2 = new Thread(west, "West Person");
+        thread.start();
+        thread2.start();
     }
 }
