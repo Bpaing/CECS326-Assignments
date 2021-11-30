50a3f327804cc43270b20d4efdd87620930af2b4
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2021-11-29 19:26:32 -0800

    FCFS first implementation
    first implementation of FCFS. Has not been tested yet.

--- Project 4/src/FCFS.java
+++ Project 4/src/FCFS.java
@@ -7,21 +7,29 @@ import java.util.*;
 //Your code here
 public class FCFS implements Algorithm
 {
+    private List<Task> queue;
 
     public FCFS(List<Task> queue)
     {
-
+        this.queue = queue;
     }
 
     @Override
     public void schedule()
     {
-
+        System.out.println("Scheduling order of FCFS:");
+        while (queue.size() != 0) {
+           Task next = pickNextTask();
+           queue.remove(next);
+           CPU.run(next, next.getBurst());
+           System.out.printf("%s, %d\n", next.getName(), next.getBurst());
+       }
     }
 
+    //FCFS chooses the first task in the queue.
     @Override
     public Task pickNextTask()
     {
-        return null;
+        return queue.get(0);
     }
 }
